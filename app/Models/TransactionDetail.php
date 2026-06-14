7c62f15098a7ca1891f61a59e1578f418360e348
alwiakbarmuz <dedealwi37@gmail.com>
2026-06-14 21:19:37 +0700

    mengedit TransactionDetail

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionDetail extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'quantity',
+        'price',
+        'subtotal'
+    ];
+
+    // Relationships
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
